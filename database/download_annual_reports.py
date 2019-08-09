5d04629f4835fc5a4957a0b17e75973a36cc5e48
Wes Galbraith <galbwe92@gmail.com>
2019-08-09 01:18:06 -0500

    download to json and create insert statement

--- database/download_annual_reports.py
+++ database/download_annual_reports.py
@@ -1,8 +1,11 @@
-import re
 import os
+import re
+from pprint import pprint
 
 import pandas as pd
 import requests
+
+import sqlalchemy as sa
 
 #shamelessly copied from stack overflow
 def download_file_from_google_drive(id, destination):
@@ -40,7 +43,8 @@ ids = {
     'projects': '19eAtW6hcA3yE0WYK-cOr0UQG_AFaxNT2'
 }
 
-def download_to_df(sheet):
+
+def download_to_json(sheet):
     id = ids.get(sheet)
     if id is None:
         raise ValueError('invalid sheet')
@@ -49,15 +53,98 @@ def download_to_df(sheet):
     try:
         filename = f'./temp/{sheet}.csv'
         download_file_from_google_drive(id, filename)
-        df = pd.read_csv(filename)
+        with open(filename, 'r') as f:
+            lines = f.readlines()
+
     finally:
         os.remove(filename)
         os.rmdir('temp')
-    return df
+
+    columns, *data = (line.strip().split(',') for line in lines)
+    objects = [dict(zip(columns, row)) for row in data]
+    return objects
+
+
+def format_location(location):
+    location = dict((k, None) if not location[k] else (k, location[k]) for k in location)
+    del location['id']
+    address = location.get('address')
+    location['address'] = address.strip() if address else None
+
+    city = location.get('city')
+    location['city'] = city.strip() if city else None
+
+    latitude = format_latitude(location['latitude'])
+    longitude = format_longitude(location['longitude'])
+    if latitude and longitude:
+        location['location'] = 'POINT({!s} {!s})'.format(latitude, longitude)
+    else:
+        return None
+
+    del location['latitude']
+    del location['longitude']
+
+    location['zip_code'] = int(location['zip_code'].split('.')[0]) if location['zip_code'] else None
+    location['state'] = state_abbreviations.get(location['state'])
+
+    return location
+
+lat_regex = re.compile(r'''(\d{1,2}).+(\d{1,2})\'(\d{1,2}\.\d{1,2})\"\"([NS])"''')
+
+def format_latitude(lat):
+    if not isinstance(lat, str):
+        return None
+    m = lat_regex.search(lat)
+    if m:
+        degrees = float(m.group(1))
+        minutes = float(m.group(2)) / 60
+        seconds = float(m.group(3)) / 60**2
+        decimal = degrees + minutes + seconds
+        return decimal if m.group(4) == 'N' else -decimal
+
+lng_regex = re.compile(r'''(\d{1,2}).+(\d{1,2})\'(\d{1,2}\.\d{1,2})\"\"([EW])"''')
+
+def format_longitude(long):
+    if not isinstance(long, str):
+        return None
+    m = lng_regex.search(long)
+    if m:
+        degrees = float(m.group(1))
+        minutes = float(m.group(2)) / 60
+        seconds = float(m.group(3)) / 60**2
+        decimal = degrees + minutes + seconds
+        return decimal if m.group(4) == 'E' else -decimal
+
+state_abbreviations = {
+    ' MO"': 'MO',
+    'Kansas': 'KS',
+    'Missouri': 'MO',
+    'Nebraska': 'NE'
+}
+
+def create_sqlalchemy_engine(connection_string=None):
+    if connection_string is None:
+        connection_string = os.environ.get('DEV_DATABASE_URL')
+        if connection_string is None:
+            raise ValueError('No connection string')
+    return sa.create_engine(connection_string)
 
 
 
+def bulk_insert(engine, table, values):
+    connection = engine.connect()
+    connection.execute(
+        table.insert(),
+        values
+    )
 
-if __name__ == '__main__':
-    locations = download_to_df('locations')
-    projects = download_to_df('projects')
+def insert_locations_08_09_2019(connection_url):
+    metadata = sa.MetaData()
+    engine = create_sqlalchemy_engine(connection_url)
+    locations_table = sa.Table('locations', metadata, autoload=True, autoload_with=engine)
+    locations = download_to_json('locations')
+    formatted = [format_location(loc) for loc in locations]
+    filtered = filter(lambda loc: loc is not None, formatted)
+    filtered = filter(lambda loc: loc.get('address') is not None, filtered)
+    pprint(list(filtered))
+    bulk_insert(locations_table, list(filtered))
