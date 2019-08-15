5f0edb2ae1b7f8564c6a24271bf6000b35632248
Wes Galbraith <galbwe92@gmail.com>
2019-08-15 00:09:26 -0500

    functionality for projects form

--- api/src/forms.py
+++ api/src/forms.py
@@ -1,4 +1,5 @@
 from flask import render_template, Blueprint, request, redirect, url_for
+from sqlalchemy import func
 from models import db, Project, ProjectType, Location
 
 forms_blueprint = Blueprint('forms', __name__)
@@ -28,11 +29,32 @@ def delete_project_type(type_id):
 @forms_blueprint.route('/forms/projects', methods=['GET', 'POST'])
 def projects_form():
     if request.method == 'GET':
+        project_name = request.args.get('projectName')
         project_types = ProjectType.query.all()
-        projects = Project.query.all()
+
+        query = Project.query
+        if project_name:
+            query = query.filter(func.lower(Project.name).startswith(project_name.lower()))
+        projects = query.all()
         return render_template('projects.html', project_types=project_types, projects=projects)
     elif request.method == 'POST':
-        pass
+        form = request.form
+        new_project = Project(
+            name=form.get('project_name'),
+            description=form.get('project_description'),
+            photo_url=form.get('project_photo_url'),
+            website_url=form.get('project_website_url'),
+            year=form.get('project_year'),
+            gge_reduced=form.get('project_gge_reduced'),
+            ghg_reduced=form.get('project_ghg_reduced'),
+        )
+        project_type_name = form.get('project_type_name')
+        if project_type_name:
+            project_type = ProjectType.query.filter_by(type_name=project_type_name).first()
+            new_project.type = project_type
+        db.session.add(new_project)
+        db.session.commit()
+        return redirect(url_for('forms.projects_form'), code=303)
     else:
         # raise method not found error
         pass
